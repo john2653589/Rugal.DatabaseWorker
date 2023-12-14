0de2bf531c4fe5846b0bff4fbbfee42b3d4a760f
Rugal <john2653589@gmail.com>
2023-12-14 22:52:57 +0800

    Rugal Update

---
+++ Rugal.DatabaseWorker.Core/Extention/StartupExtention.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rugal.DatabaseWorker.Core.Service;
+
+namespace Rugal.DatabaseWorker.Core.Extention
+{
+    public static class StartupExtention
+    {
+        public static IServiceCollection AddDatabaseWorker(this IServiceCollection Services)
+        {
+            Services.AddScoped<DbWorker>();
+            return Services;
+        }
+    }
+}
